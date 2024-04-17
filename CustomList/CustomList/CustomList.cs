fc404966747e5e0e42e1c845d4dac8d40a942982
karimlifatimaa <bd7tzqw95@code.edu.az>
2024-04-18 02:24:48 +0400

    CustomList

---
+++ CustomList/CustomList/CustomList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    internal class CustomList<T>
+    {
+
+        T[] _items;
+        public int Capacity;
+        public T this[int index]
+        {
+            get { return _items[index]; }
+        }
+
+        public int Count;
+        public CustomList()
+        {
+            Capacity = 4;
+            _items = new T[Capacity];
+            Count = 0;
+        }
+        public void Add(T item)
+        {
+            if (Count == Capacity)
+            {
+                Capacity *= 2; 
+                Array.Resize(ref _items, Capacity);
+            }
+            _items[Count] = item;
+            Count++;
+        }
+        public T Find(Predicate<T> method)
+        {
+            foreach (T item in _items)
+            {
+                if (method(item))
+                {
+                    return item;
+                }
+            }
+            return default(T);
+        }
+
+        public CustomList<T> FindAll(Predicate<T> method)
+        {
+            CustomList<T> list = new CustomList<T>();
+            foreach(T item in _items)
+            {
+                if (method(item))
+                {
+                    list.Add(item);
+
+                }
+            }
+            return list;
+        }
+
+
+        public void ForEach(Action<T> action)
+        {
+            foreach(T a in _items)
+            {
+                action(a);
+            }
+        }
+        public bool Contains(T item)
+        {
+            return _items.Contains(item);
+        }
+
+    }
+}
---
+++ CustomList/CustomList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Channels;
+
+namespace CustomList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList<int> myList = new CustomList<int>();
+
+          
+            myList.Add(12);
+            myList.Add(22);
+            myList.Add(33);
+            myList.Add(42);
+            myList.Add(5);
+
+
+            Console.WriteLine("Capasity"+ myList.Capacity) ;
+
+            Console.WriteLine("Elementler:");
+            myList.ForEach(x=>Console.WriteLine(x));
+
+            int foundItem = myList.Find(x => x == 42);
+            Console.WriteLine("Tapilan deyer: " + foundItem);
+
+            int fountItem = myList.Find(x => x == 12);
+            Console.WriteLine("Tapildi:" + fountItem);
+
+            CustomList<int> evenNumbers = myList.FindAll(x => x % 2 == 0);
+            Console.WriteLine("Cut ededler:");
+            evenNumbers.ForEach(x => Console.WriteLine(x));
+
+           
+            Console.WriteLine(myList.Contains(2));
+        }
+    }
+}
